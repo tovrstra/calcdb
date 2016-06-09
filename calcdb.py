8a54564eee94d5bb8539a29b93bbc51208d05756
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-06-09 04:15:54 +0200

    Add populations

--- calcdb.py
+++ calcdb.py
@@ -437,6 +437,7 @@ class HDF5ChargeFields(HDF5Fields):
             HDF5FieldInfo('estruct/valence_charges/%s' % scheme, (), 'atom', float, 'valence_charges'),
             HDF5FieldInfo('estruct/valence_widths/%s' % scheme, (), 'atom', float, 'valence_widths'),
             HDF5FieldInfo('estruct/core_charges/%s' % scheme, (), 'atom', float, 'core_charges'),
+            HDF5FieldInfo('estruct/populations/%s' % scheme, (), 'atom', float, 'populations'),
         ])
 
 
